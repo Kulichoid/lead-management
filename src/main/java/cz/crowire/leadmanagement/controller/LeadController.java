2a641cb0417db319a0267c84757889080f57a621
Lukáš Vrána <lukas.vrana@vodafone.com>
2024-10-07 21:57:13 +0200

    Add logging to LeadController for better debugging and monitoring

--- src/main/java/cz/crowire/leadmanagement/controller/LeadController.java
+++ src/main/java/cz/crowire/leadmanagement/controller/LeadController.java
@@ -2,6 +2,7 @@ package cz.crowire.leadmanagement.controller;
 
 import cz.crowire.leadmanagement.entity.Lead;
 import cz.crowire.leadmanagement.repository.LeadRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,28 +13,33 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/leads")
+@Slf4j
 public class LeadController {
 
   @Autowired private LeadRepository leadRepository;
 
   @GetMapping
   public List<Lead> getAllLeads() {
+    log.info("Fetching all leads");
     return leadRepository.findAll();
   }
 
   @GetMapping("/{id}")
   public ResponseEntity<Lead> getLeadById(@PathVariable Long id) {
+    log.info("Fetching lead with ID: {}", id);
     Optional<Lead> lead = leadRepository.findById(id);
     return lead.map(ResponseEntity::ok).orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
   }
 
   @PostMapping
   public Lead createLead(@RequestBody Lead lead) {
+    log.info("Creating new lead: {}", lead);
     return leadRepository.save(lead);
   }
 
   @PutMapping("/{id}")
   public ResponseEntity<Lead> updateLead(@PathVariable Long id, @RequestBody Lead leadDetails) {
+    log.info("Updating lead with ID: {}", id);
     Optional<Lead> leadOptional = leadRepository.findById(id);
 
     if (leadOptional.isPresent()) {
@@ -49,10 +55,13 @@ public class LeadController {
 
   @DeleteMapping("/{id}")
   public ResponseEntity<Void> deleteLead(@PathVariable Long id) {
+    log.info("Deleting lead with ID: {}", id);
     if (leadRepository.existsById(id)) {
       leadRepository.deleteById(id);
+      log.info("Lead with ID: {} deleted", id);
       return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     } else {
+      log.warn("Lead with ID: {} not found", id);
       return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
   }
